d60ffbc7d97f1dbc466518b23efac5b348836058
Jenelle Holloway <hollowayjenelle@gmail.com>
2024-02-26 22:12:01 -0500

    pass data through stack navigation screens

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -2,10 +2,12 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { globalStyles } from "../styles/global";
 
-const ReviewDetails = ({ navigation }) => {
+const ReviewDetails = ({ navigation, route }) => {
+  const { title, body } = route.params;
   return (
     <View style={globalStyles.container}>
-      <Text>Home Page</Text>
+      <Text>{title}</Text>
+      <Text>{body}</Text>
     </View>
   );
 };
