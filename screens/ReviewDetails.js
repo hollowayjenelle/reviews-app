3d6cf4f23e4440b4a24c3b19b5877f56369c95c9
Jenelle Holloway <hollowayjenelle@gmail.com>
2024-03-06 20:46:17 -0500

    update ReviewDetails

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { globalStyles } from "../styles/global";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
+import Card from "../shared/card";
 
-const ReviewDetails = ({ navigation, route }) => {
-  const { title, body } = route.params;
+const ReviewDetails = ({ route }) => {
+  const { title, body, rating } = route.params;
   return (
     <View style={globalStyles.container}>
-      <Text>{title}</Text>
-      <Text>{body}</Text>
+      <Card>
+        <Text>{title}</Text>
+        <Text>{body}</Text>
+        <View>
+          <Text>GamerInsight rating:</Text>
+          <Image source={images.ratings[rating]} />
+        </View>
+      </Card>
     </View>
   );
 };
